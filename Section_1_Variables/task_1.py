64305584f144a922373dd44dce88012748a9d2a3
Tanzim Emon <e.tanzim98@gmail.com>
2026-04-22 12:45:25 +0600

    Task 1

---
+++ Section_1_Variables/task_1.py
@@ -0,0 +1,19 @@
+# Assign multiple variables in one line
+name, age, city = "Tanzim", 27, "Dhaka"
+
+# Print values
+print("User name is" , name)
+print(f"{name}'s age is " ,age)
+print(f"{name} lives in" ,city)
+
+print()
+
+
+# Assign same value for multiple variables
+
+A = B = C= D = 20
+
+print("value of variable A is:" ,A)
+print("value of variable B is:" ,B)
+print("value of variable C is:", C)
+print("value of variable D is:", D)
