84736fa3016d6de6e3cb4eb6eaffa5729dc5ea2d
Tanzim Emon <e.tanzim98@gmail.com>
2026-04-29 13:06:07 +0600

    Section 8,9 & 10

---
+++ Assignment_2/task_1.py
@@ -0,0 +1,15 @@
+# Python Program to Check if a Number is Prime (using function)
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
---
+++ Assignment_2/task_2.py
@@ -0,0 +1,11 @@
+# Python Program to Calculate Factorial of a Number (using function)
+
+def factorial(num):
+    result = 1
+    for i in range(1, num + 1):
+        result = result * i
+    return result
+
+number = int(input("Enter a number: "))
+
+print("Factorial =", factorial(number))
